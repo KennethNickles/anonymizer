1f4e00f7c986eaa7112a912052fd0499758c8d3a
Kenneth Nickles <kenneth.nickles@workday.com>
2018-08-21 12:54:58 -0700

    added anonymized without whitespaced file name support

---
+++ anonymizer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# install python3
+# link python3
+# download conda 3.6
+# install conda 3.6
+
+import sys
+import hashlib
+import binascii
+import base64
+import pandas as pd
+import numpy as np
+
+salt = "bluegreenredwhiteblack"
+
+def main():
+	fileName = sys.argv[1]
+	xlsx = pd.ExcelFile(fileName)
+	df = pd.read_excel(xlsx, xlsx.sheet_names[0])
+	for column in df.columns:
+		if column == "lastname" or column == "firstname":
+			df[column] = ""
+		if column == "identityname":
+			index = 0
+			for value in df[column]:
+				dk = hashlib.pbkdf2_hmac('sha1', str.encode(value) , str.encode(salt), 100000)
+				dk_readable = base64.urlsafe_b64encode(dk)
+				print(dk_readable)
+				df[column][index] = dk_readable
+				index += 1
+
+	df.to_excel(fileName[:fileName.find('.')] + "_anonymized.xlsx", sheet_name=xlsx.sheet_names[0])
+
+
+if __name__ == '__main__':
+	main()
