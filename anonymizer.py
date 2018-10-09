004a5fa9ae23e692f99897179a1c4ea2535b7201
Kenneth Nickles <kenneth.nickles@workday.com>
2018-10-09 16:42:11 -0700

    added support for Rafe's new csv template

--- anonymizer.py
+++ anonymizer.py
@@ -14,6 +14,9 @@ import numpy as np
 import csv
 
 defaultSalt = "bluegreenredwhiteblack"
+username = "Username"
+firstname = "FirstName"
+lastname = "LastName"
 
 def main():
 	fileName = sys.argv[1]
@@ -22,48 +25,34 @@ def main():
 	salt = defaultSalt
 	if len(sys.argv) > 2:
 		salt = sys.argv[2]
-	caches = {}
 	if fileExtension == ".csv":
-		with open(fileName, 'r', encoding="utf8") as readfile:
-			reader = csv.reader(readfile, delimiter=',', quoting=csv.QUOTE_ALL)
-			with open(fileName[:fileName.find('.')] + "_anonymized.csv", "w", encoding="utf8", newline='') as writefile:
-				writer = csv.writer(writefile, delimiter=',', quoting=csv.QUOTE_ALL)
-				index = 0
-				newRows = []
-				for row in reader:
-					newRow = [row[0], row[1], row[2], row[3], row[4]]
-					if index != 0:
-						dk_readable = b"unknown"
-						if row[2] in caches.keys():
-							dk_readable = caches[row[2]]
-						else:
-							dk = hashlib.pbkdf2_hmac('sha1', str.encode(row[2]) , str.encode(salt), 100)
-							dk_readable = binascii.hexlify(dk)
-							caches[row[2]] = dk_readable
-						newRow[0] = ""
-						newRow[1] = ""
-						newRow[2] = dk_readable
-						print(newRow)
-					writer.writerow(newRow)
-					index += 1
-	elif fileExtension == ".xlsx":
-		file = pd.ExcelFile(fileName)
-		df = pd.read_excel(file, file.sheet_names[0])
-		for column in df.columns:
-			if column == "lastname" or column == "firstname":
-				df[column] = ""
-			if column == "identityname":
-				index = 0
-				for value in df[column]:
-					dk = hashlib.pbkdf2_hmac('sha1', str.encode(value) , str.encode(salt), 100000)
-					dk_readable = base64.urlsafe_b64encode(dk)
-					print(dk_readable)
-					df[column][index] = dk_readable
-					index += 1
-
-		df.to_excel(fileName[:fileName.find('.')] + "_anonymized.xlsx", sheet_name=xlsx.sheet_names[0])
+		parseCsv(fileName, salt)
 	else:
 		raise Exception("unrecognized file type" + fileExtension)
+
+def parseCsv(fileName, salt):
+	with open(fileName, 'r', encoding="utf8") as readfile, open(fileName[:fileName.find('.')] + "_anonymized.csv", "w", encoding="utf8", newline='') as writefile:
+		reader = csv.DictReader(readfile)
+		writer = csv.DictWriter(writefile, fieldnames=reader.fieldnames)
+		caches = {}
+		newRows = []
+		index = 0
+		for row in reader:
+			newRow = row.copy()
+			if index != 0:
+				dk_readable = b"unknown"
+				if newRow[username] in caches.keys():
+					dk_readable = caches[row[username]]
+				else:
+					dk = hashlib.pbkdf2_hmac('sha1', str.encode(row[username]) , str.encode(salt), 100)
+					dk_readable = binascii.hexlify(dk)
+					caches[row[username]] = dk_readable
+				newRow[username] = dk_readable.decode("utf-8") 
+				newRow[firstname] = ""
+				newRow[lastname] = ""
+				print(newRow)
+			writer.writerow(newRow)
+			index += 1
 
 if __name__ == '__main__':
 	main()
